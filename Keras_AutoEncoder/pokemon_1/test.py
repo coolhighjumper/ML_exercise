94d758603aec7f38821d94ad54695a7cb1d5161d
Chiang <chiang@Chiangde-MacBook-Pro.local>
2018-01-13 13:38:40 +0800

    create pokemon vae3

--- Keras_AutoEncoder/pokemon_1/test.py
+++ Keras_AutoEncoder/pokemon_1/test.py
@@ -27,14 +27,14 @@ from keras import backend as K
 
 input_img=Input(shape=(32,32,4))
 
-conv1=Conv2D(32,(2,2),activation='relu',padding='same',strides=1)(input_img)
+conv1=Conv2D(64,(2,2),activation='relu',padding='same',strides=1)(input_img)
 #maxpool1=MaxPooling2D((2,2),padding='same')(conv1)
-conv2=Conv2D(32,(2,2),activation='relu',padding='same',strides=1)(conv1)
+conv2=Conv2D(64,(2,2),activation='relu',padding='same',strides=1)(conv1)
 #maxpool2=MaxPooling2D((2,2),padding='same')(conv2)
-conv3=Conv2D(32,(2,2),activation='relu',padding='same',strides=1)(conv2)
+conv3=Conv2D(64,(2,2),activation='relu',padding='same',strides=1)(conv2)
 maxpool3=MaxPooling2D((2,2),padding='same')(conv3)
 flat=Flatten()(maxpool3)
-dense1=Dense(16*16)(flat)
+dense1=Dense(16*16*8)(flat)
 dense2=Dense(16*8)(dense1)
 z_mean=Dense(32)(dense2)
 z_log_var=Dense(32)(dense2)
@@ -49,18 +49,20 @@ encoder=Model(input_img,z)
 
 decoder_input=Input(shape=(32,))
 dec_dense1=Dense(16*8)
-dec_dense2=Dense(16*16*32)
-dec_reshape=Reshape((16,16,32))
-dec_conv1=Conv2DTranspose(32,(3,3),activation='relu',padding='same',strides=1)
+dec_dense2=Dense(16*16*8)
+dec_dense3=Dense(16*16*64)
+dec_reshape=Reshape((16,16,64))
+dec_conv1=Conv2DTranspose(64,(3,3),activation='relu',padding='same',strides=1)
 #dec_ups1=UpSampling2D((2,2))
-dec_conv2=Conv2DTranspose(32,(3,3),activation='relu',padding='same',strides=1)
+dec_conv2=Conv2DTranspose(64,(3,3),activation='relu',padding='same',strides=1)
 #dec_ups2=UpSampling2D((2,2))
-dec_conv3=Conv2DTranspose(32,(3,3),activation='relu',padding='same',strides=1)
+dec_conv3=Conv2DTranspose(64,(3,3),activation='relu',padding='same',strides=1)
 dec_ups3=UpSampling2D((2,2))
 dec_conv4=Conv2D(4,(3,3),activation='sigmoid',padding='same',strides=1)
 
 x=dec_dense1(z)
 x=dec_dense2(x)
+x=dec_dense3(x)
 x=dec_reshape(x)
 x=dec_conv1(x)
 #x=dec_ups1(x)
@@ -72,6 +74,7 @@ x=dec_conv4(x)
 
 decoder=dec_dense1(decoder_input)
 decoder=dec_dense2(decoder)
+decoder=dec_dense3(decoder)
 decoder=dec_reshape(decoder)
 decoder=dec_conv1(decoder)
 #decoder=dec_ups1(decoder)
@@ -111,12 +114,12 @@ vae = Model(input_img, y)
 vae.compile(optimizer='RMSprop', loss=None)
 vae.summary()
 
-from keras.models import model_from_json
-vae.load_weights('./vae1_weight.h5')
-
-vae.fit(x=x_train,shuffle=True,epochs=3000,validation_data=(x_test,None),batch_size=32)
+'''from keras.models import model_from_json
+vae.load_weights('./vae3_weight.h5')
+'''
+vae.fit(x=x_train,shuffle=True,epochs=1000,validation_data=(x_test,None),batch_size=32)
 
 
 import json
 from keras.applications import imagenet_utils
-vae.save_weights('./vae2_weight.h5')
+vae.save_weights('./vae3_weight.h5')
